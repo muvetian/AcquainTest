d1bc6b24444c0a80fa01c0cd6801b33ae4d4e999
Binwei Xu <bixu@Binweis-MacBook-Pro.local>
2017-10-01 19:43:14 -0700

    reorganize

--- testLogin/Pods/Target Support Files/Pods-testLoginUITests/Pods-testLoginUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_testLoginUITests : NSObject
-@end
-@implementation PodsDummy_Pods_testLoginUITests
-@end
--- testLogin/testLogin/header.m
+++
@@ -1,9 +0,0 @@
-//
-//  header.m
-//  testLogin
-//
-//  Created by Mutian on 2/21/17.
-//  Copyright © 2017 Team3. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
